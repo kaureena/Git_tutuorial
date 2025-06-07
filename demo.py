14ae2b196edf19b8e78d4fe9f51a96452a10687a
Reena <chauhanreenabahen1990@gmail.com>
2025-06-07 22:04:00 +0100

    First v1 version

---
+++ demo.py
@@ -0,0 +1,29 @@
+# # # x = x "5"
+# # # if x > "4"
+# # #     result = "true"
+# # #     result = "false"
+# # # print(result)
+
+
+# # a = 3 
+# # b = 2
+# # if a > b:
+# #     c = a - b
+# # print(c)
+
+# a = "5"
+# b = "4"
+# c = a + b
+# print(c)
+# x = [1, 2, 3]
+# print(x[:-1])
+# a = 28
+# b = '22'
+# print(int(b) + int(a))
+# num = 1234
+# rev = str(num)[::1]
+# print(rev)
+def func(x):
+    return x * 2
+y=3
+print(func(y))
